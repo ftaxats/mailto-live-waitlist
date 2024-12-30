f90b9edb8e73ad837713d8a93af66b9856efb8db
Akshat <70918043+ftaxats@users.noreply.github.com>
2024-12-30 21:39:32 +0530

    page.tsx edit

--- app/page.tsx
+++ app/page.tsx
@@ -119,6 +119,7 @@ export default function Home() {
           handleEmailChange={handleEmailChange}
           handleSubmit={handleSubmit}
           loading={loading}
+        />
       </section>
       <Footer />
       <Particles
